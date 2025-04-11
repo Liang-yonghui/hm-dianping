0fb0068715094a493f3a405d2b2a33ac1a4d96cd
梁永辉 <925277632@qq.com>
2025-04-11 17:48:03 +0800

    Redis实战-短信登录

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -17,12 +17,7 @@ import javax.servlet.http.HttpSession;
  */
 public interface IUserService extends IService<User> {
 
-    Result sendCode(String phone, HttpSession session);
-
     Result login(LoginFormDTO loginForm, HttpSession session);
 
-    Result sign();
-
-    Result signCount();
-
+    Result sentCode(String phone, HttpSession session);
 }
